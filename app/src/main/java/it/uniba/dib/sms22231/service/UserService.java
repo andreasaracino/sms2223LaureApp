0cdf223f66f43700fcfe63a49021fbd2a3cc34fa
Andrea Saracino <andrea.saracino20@gmail.com>
2023-08-29 10:56:49 +0200

    commented user service

--- app/src/main/java/it/uniba/dib/sms22231/service/UserService.java
+++ app/src/main/java/it/uniba/dib/sms22231/service/UserService.java
@@ -12,18 +12,24 @@ import it.uniba.dib.sms22231.model.User;
 import it.uniba.dib.sms22231.utility.Observable;
 import it.uniba.dib.sms22231.utility.CallbackFunction;
 
+// SERVICE SINGLETON
 public class UserService {
     static UserService instance;
-    private FirebaseFirestore db;
-    private FirebaseAuth mAuth;
-    private FirebaseUser user;
-    private DocumentReference userDocument;
-    public final Observable<User> userObservable = new Observable<>(null);
+    private FirebaseFirestore db;   // Istanza database Firestore
+    private FirebaseAuth mAuth;     // Istanza gestore autenticazione
+    private FirebaseUser user;      // Istanza utente corrente
+    private DocumentReference userDocument; // Riferimento al documento sul database
+    public final Observable<User> userObservable = new Observable<>(null);  // Observable contenente i dati dell'utente
 
     private UserService() {
         initData();
     }
 
+    /*
+    * creazione dell'istanza di FirebaseFirestore
+    * creazione dell'istanza di FirebaseAuth
+    * se l'utente è loggato, allora ottieni i suoi dati
+    */
     private void initData() {
         db = FirebaseFirestore.getInstance();
         mAuth = FirebaseAuth.getInstance();
@@ -33,6 +39,12 @@ public class UserService {
         }
     }
 
+    /*
+     * accesso alla collection "users" e ottenimento del documento con id uguale a quello dell'utente loggato
+     * una volta ottenuti i dati, si controlla se l'operazione è andata a buon fine
+     * in tal caso si crea un nuovo oggetto di tipo User in cui viene passato il documento ottenuto
+     * successivamente si passano anche UID ed email dell'utente loggato, oltre ad aggiornare l'observable
+     */
     private void updateDocuments() {
         userDocument = db.collection("users").document(user.getUid());
         userDocument.get().addOnCompleteListener(task -> {
@@ -53,10 +65,18 @@ public class UserService {
         });
     }
 
+    /*
+     * i dati aggiornati dell'utente si salvano direttamente nel documento ottenuto in initData
+     * a task completato si segnala, attraverso il callback, la buona riuscita dell'operazione al metodo chiamante
+     */
     public void saveUserData(User user, CallbackFunction<Boolean> callback) {
         userDocument.set(user).addOnCompleteListener(task -> callback.apply(task.isSuccessful()));
     }
 
+    /*
+     * si effettua il login attraverso l'uso di email e password inseriti dall'utente
+     * una volta terminata l'operazione viene chiamato initData e successivamente la callback per segnalare l'esito dell'operazione
+     */
     public void signIn(String email, String password, CallbackFunction<Boolean> callback) {
         if (!email.isEmpty() && !password.isEmpty()) {
             mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(task -> {
@@ -68,10 +88,16 @@ public class UserService {
         }
     }
 
+    /*
+     * metodo per ottenere il valore corrente dell'observable
+     */
     public User getUserData() {
         return userObservable.getValue();
     }
 
+    /*
+     * metodo statico per la creazione di un'istanza secondo il pattern singleton
+     */
     public static UserService getInstance() {
         if (instance == null) {
             instance = new UserService();
