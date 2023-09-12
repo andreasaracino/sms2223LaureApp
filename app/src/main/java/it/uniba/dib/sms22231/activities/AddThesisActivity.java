795b35846edf24596eae3a04cbcb8ea4563068eb
MarcelloPit <marcpit85@hotmail.it>
2023-09-12 17:15:35 +0200

    implemented AddThesisActivity with requirements

--- app/src/main/java/it/uniba/dib/sms22231/activities/AddThesisActivity.java
+++ app/src/main/java/it/uniba/dib/sms22231/activities/AddThesisActivity.java
@@ -5,14 +5,11 @@ import androidx.activity.result.ActivityResultCallback;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
 
-import android.Manifest;
 import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
@@ -20,11 +17,9 @@ import android.provider.OpenableColumns;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
-import android.widget.Toast;
-
-import com.google.android.gms.dynamic.IFragmentWrapper;
+import android.widget.Spinner;
 
 import java.util.ArrayList;
 
@@ -33,12 +28,14 @@ import it.uniba.dib.sms22231.R;
 public class AddThesisActivity extends AppCompatActivity {
 
     private ActivityResultLauncher<Intent> resultLauncher;
-    private ListView listView;
+    private ListView listViewFile;
+    private ListView listViewReq;
     private ArrayList<String> fileName;
+    private ArrayList<String> requirement;
     private ArrayAdapter<String> listadapter;
 
 
-     protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_thesis);
 
@@ -46,39 +43,41 @@ public class AddThesisActivity extends AppCompatActivity {
         actionBar.setTitle(R.string.addThesis);
         actionBar.setDisplayHomeAsUpEnabled(true);
 
-        listView = findViewById(R.id.fileListView);
+        listViewFile = findViewById(R.id.fileListView);
         fileName = new ArrayList<String>();
+        listViewReq = findViewById(R.id.reqListView);
+        requirement = new ArrayList<String>();
 
         resultLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), new ActivityResultCallback<ActivityResult>() {
             @Override
             public void onActivityResult(ActivityResult result) {
                 Intent data = result.getData();
-                if (data != null){
+                if (data != null) {
                     Uri uri = data.getData();
-                    String name = getNameFromUri (uri);
+                    String name = getNameFromUri(uri);
                     fileName.add(name);
-                    fillList();
+                    fillList(fileName, listViewFile);
                 }
             }
         });
     }
 
-    private void fillList() {
-         listadapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,fileName);
-         listView.setAdapter(listadapter);
+    private void fillList(ArrayList<String> arrayList, ListView listView) {
+        listadapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, arrayList);
+        listView.setAdapter(listadapter);
     }
 
     private String getNameFromUri(Uri uri) {
-        Cursor cursor = getContentResolver().query(uri, null,null,null);
+        Cursor cursor = getContentResolver().query(uri, null, null, null);
         int nameIndex = cursor.getColumnIndex(OpenableColumns.DISPLAY_NAME);
         cursor.moveToFirst();
         return cursor.getString(nameIndex);
     }
 
-    public void addFile (View view){
-         Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
-         intent.setType("*/*");
-         resultLauncher.launch(intent);
+    public void addFile(View view) {
+        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+        intent.setType("*/*");
+        resultLauncher.launch(intent);
     }
 
     @Override
@@ -89,6 +88,34 @@ public class AddThesisActivity extends AppCompatActivity {
                 return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    public void addRequirement(View view) {
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        final View customLayout = getLayoutInflater().inflate(R.layout.custom_alert, null);
+        builder.setView(customLayout);
+
+        builder.setPositiveButton("OK", (dialog, which) -> {
+            String req = null;
+            Spinner spinner = customLayout.findViewById(R.id.reqSpinner);
+            EditText editText = customLayout.findViewById(R.id.reqEdit);
+            int position = spinner.getSelectedItemPosition();
+            switch (position) {
+                case 0:
+                    req = (getString(R.string.average) + ": " + (editText.getText().toString()));
+                    break;
+                case 1:
+                    req = (getString(R.string.exam) + ": " + (editText.getText().toString()));
+            }
+            if (req != null) {
+                requirement.add(req);
+                fillList(requirement, listViewReq);
+            }
+        });
+
+        AlertDialog dialog = builder.create();
+        dialog.show();
     }
 
 }
