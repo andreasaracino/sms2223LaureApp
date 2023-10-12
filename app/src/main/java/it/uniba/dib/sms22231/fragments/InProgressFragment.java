f75f165a5f811d720437ab686f753a0d9fc12cc5
MarcelloPit <marcpit85@hotmail.it>
2023-10-12 02:42:14 +0200

    implemented InProgressFragment

--- app/src/main/java/it/uniba/dib/sms22231/fragments/InProgressFragment.java
+++ app/src/main/java/it/uniba/dib/sms22231/fragments/InProgressFragment.java
@@ -1,20 +1,73 @@
 package it.uniba.dib.sms22231.fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 import it.uniba.dib.sms22231.R;
+import it.uniba.dib.sms22231.activities.ApplicationDetailActivity;
+import it.uniba.dib.sms22231.activities.MyThesisActivity;
+import it.uniba.dib.sms22231.adapters.RecyclerAdapter;
+import it.uniba.dib.sms22231.config.ApplicationStatus;
+import it.uniba.dib.sms22231.model.Application;
+import it.uniba.dib.sms22231.model.CardData;
+import it.uniba.dib.sms22231.service.ApplicationService;
+import it.uniba.dib.sms22231.utility.RecyclerViewInterface;
 
-public class InProgressFragment extends Fragment {
+public class InProgressFragment extends Fragment implements RecyclerViewInterface {
+    private final ApplicationService applicationService = ApplicationService.getInstance();
+    private View view;
+    private ArrayList<CardData<Application>> cardDataArrayList;
+    private TextView noItemText;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_in_progress, container, false);
+
+        view = inflater.inflate(R.layout.fragment_in_progress, container, false);
+        noItemText = view.findViewById(R.id.noThesisFoundText);
+
+        getApplications();
+
+        return view;
+    }
+
+    private void getApplications() {
+        applicationService.getAllApplicationsByStatus(ApplicationStatus.approved).subscribe(applications -> {
+            cardDataArrayList = new ArrayList<>();
+            applications.forEach(application -> {
+                CardData<Application> cardData = new CardData<>(application.studentName, application.thesisTitle, application.id, null, application);
+                cardDataArrayList.add(cardData);
+            });
+            RecyclerView rec = view.findViewById(R.id.accepetdApplicationsRecycler);
+            RecyclerAdapter<Application> recad = new RecyclerAdapter<>(cardDataArrayList, getContext(), this);
+            LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getContext(), LinearLayoutManager.VERTICAL, false);
+            rec.setLayoutManager(linearLayoutManager);
+            rec.setAdapter(recad);
+            if (cardDataArrayList.isEmpty()){
+                noItemText.setVisibility(View.VISIBLE);
+            } else {
+                noItemText.setVisibility(View.GONE);
+            }
+        });
+    }
+
+    @Override
+    public void onItemClick(int position) {
+        Intent intent = new Intent(getContext(), MyThesisActivity.class);
+        String id = cardDataArrayList.get(position).getData().id;
+        intent.putExtra("id",id);
+        int caller = 4;
+        intent.putExtra("caller", caller);
+        startActivity(intent);
     }
 }
