4275e005fff5d66e513d78f81b8b65b30a484bc9
MarcelloPit <marcpit85@hotmail.it>
2023-10-18 12:14:12 +0200

    added refresh in AvailableFragment

--- app/src/main/java/it/uniba/dib/sms22231/fragments/AvailableFragment.java
+++ app/src/main/java/it/uniba/dib/sms22231/fragments/AvailableFragment.java
@@ -7,6 +7,7 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import android.view.LayoutInflater;
 import android.view.MenuItem;
@@ -223,6 +224,14 @@ public class AvailableFragment extends Fragment implements RecyclerViewInterface
                 LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getContext(), LinearLayoutManager.VERTICAL, false);
                 rec.setLayoutManager(linearLayoutManager);
                 rec.setAdapter(recad);
+                SwipeRefreshLayout swipeRefreshLayout = view.findViewById(R.id.refreshAvailable);
+                swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+                    @Override
+                    public void onRefresh() {
+                        swipeRefreshLayout.setRefreshing(false);
+                        getTheses();
+                    }
+                });
             }
         });
     }
@@ -242,18 +251,4 @@ public class AvailableFragment extends Fragment implements RecyclerViewInterface
         startActivity(intent);
     }
 
-    @Override
-    public void onPause() {
-        super.onPause();
-        paused = true;
-    }
-
-    @Override
-    public void onResume() {
-        super.onResume();
-
-        if (paused) {
-            getTheses();
-        }
-    }
 }
