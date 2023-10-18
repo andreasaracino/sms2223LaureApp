33f1a57813a6652aefa17978529f03fe56e6c22f
MarcelloPit <marcpit85@hotmail.it>
2023-10-18 23:10:23 +0200

    implemented addition of new meetings

---
+++ app/src/main/java/it/uniba/dib/sms22231/activities/AddMeeting.java
@@ -0,0 +1,159 @@
+package it.uniba.dib.sms22231.activities;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.TimePicker;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+
+import it.uniba.dib.sms22231.R;
+import it.uniba.dib.sms22231.model.Meeting;
+import it.uniba.dib.sms22231.model.Task;
+import it.uniba.dib.sms22231.service.MeetingService;
+import it.uniba.dib.sms22231.service.TaskService;
+
+public class AddMeeting extends AppCompatActivity {
+    private final TaskService taskService = TaskService.getInstance();
+    private final MeetingService meetingService = MeetingService.getInstance();
+    private ArrayList<Task> taskArrayList;
+    private ArrayList<Task> checkedTasksArrayList = new ArrayList<>();
+    private String[] tasksArray;
+    private String[] taskIdsArray;
+    private String applicationId;
+    private boolean[] checked;
+    private TextView title;
+    private DatePicker datePicker;
+    private TimePicker timePicker;
+    private Button addTask;
+    private Button saveButton;
+    private ListView taskListView;
+    private final Calendar calendar = Calendar.getInstance();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_meeting);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setTitle(R.string.addMeeting);
+        actionBar.setDisplayHomeAsUpEnabled(true);
+
+        Intent intent = getIntent();
+        applicationId = intent.getStringExtra("applicationId");
+
+        title = findViewById(R.id.meetingTitleText);
+        datePicker = findViewById(R.id.meetingDate);
+        timePicker = findViewById(R.id.meetingTime);
+        addTask = findViewById(R.id.addTaskBtn);
+        taskListView = findViewById(R.id.taskList);
+        saveButton = findViewById(R.id.saveMeetingButton);
+
+        int day = calendar.get(Calendar.DAY_OF_MONTH);
+        int month = calendar.get(Calendar.MONTH);
+        int year = calendar.get(Calendar.YEAR);
+        datePicker.init(year, month, day, null);
+
+        timePicker.setIs24HourView(true);
+
+        addTasks();
+        saveMeeting();
+
+    }
+
+    private void saveMeeting() {
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Meeting meeting = new Meeting();
+                meeting.title = title.getText().toString();
+                calendar.set(datePicker.getYear(), datePicker.getMonth(), datePicker.getDayOfMonth(), timePicker.getHour(), timePicker.getMinute(), 0);
+                long milliseconds = calendar.getTimeInMillis();
+                meeting.date = new Date(milliseconds);
+                meeting.taskId = new ArrayList<>();
+                for (Task t : checkedTasksArrayList){
+                    meeting.taskId.add(t.id);
+                }
+                meetingService.saveNewMeeting(meeting, isSuccessfully -> {
+                    Toast.makeText(AddMeeting.this, getString(R.string.success), Toast.LENGTH_SHORT).show();
+                });
+                finish();
+            }
+        });
+    }
+
+    private void addTasks() {
+        addTask.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(AddMeeting.this);
+                builder.setTitle(getString(R.string.addTask))
+                        .setNegativeButton(getString(R.string.Cancel), null)
+                        .setPositiveButton("Ok", null);
+                taskArrayList = new ArrayList<>();
+                taskService.getTasksByApplicationId(applicationId).subscribe(tasks -> {
+                    taskArrayList.addAll(tasks);
+                    tasksArray = new String[taskArrayList.size()];
+                    taskIdsArray = new String[taskArrayList.size()];
+                    for (int i = 0; i < taskArrayList.size(); i++) {
+                        tasksArray[i] = taskArrayList.get(i).title;
+                        taskIdsArray[i] = taskArrayList.get(i).id;
+                    }
+                    checked = new boolean[taskArrayList.size()];
+                    Arrays.fill(checked, false);
+                    builder.setMultiChoiceItems(tasksArray, checked, (dialogInterface, i, b) -> checked[i] = b);
+                    AlertDialog dialog = builder.create();
+                    dialog.show();
+                    dialog.getButton(AlertDialog.BUTTON_POSITIVE).setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            for (int i = 0; i < taskArrayList.size(); i++) {
+                                if (checked[i]) {
+                                    checkedTasksArrayList.add(taskArrayList.get(i));
+                                }
+                            }
+                            fillTaskList();
+                            dialog.dismiss();
+                        }
+                    });
+                });
+
+            }
+        });
+    }
+
+    private void fillTaskList() {
+        ArrayList<String> tasksTitle = new ArrayList<>();
+        for (Task t : checkedTasksArrayList) {
+            tasksTitle.add(t.title);
+        }
+        ArrayAdapter<String> listAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, tasksTitle);
+        taskListView.setAdapter(listAdapter);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                this.finish();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
--- app/src/main/java/it/uniba/dib/sms22231/fragments/MeetingFragment.java
+++ app/src/main/java/it/uniba/dib/sms22231/fragments/MeetingFragment.java
@@ -1,5 +1,8 @@
 package it.uniba.dib.sms22231.fragments;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,60 +10,58 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.TimePicker;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Calendar;
 
 import it.uniba.dib.sms22231.R;
+import it.uniba.dib.sms22231.activities.AddMeeting;
+import it.uniba.dib.sms22231.model.Task;
+import it.uniba.dib.sms22231.service.TaskService;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link MeetingFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class MeetingFragment extends Fragment {
+     private View view;
+     private FloatingActionButton addMeetingButton;
+     private String applicationId;
+     private int caller;
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public MeetingFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment MeetingFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static MeetingFragment newInstance(String param1, String param2) {
-        MeetingFragment fragment = new MeetingFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
-
+     public MeetingFragment(String applicationId, int caller){
+         super();
+         this.applicationId = applicationId;
+         this.caller = caller;
+     }
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_meeting, container, false);
+
+        view = inflater.inflate(R.layout.fragment_meeting, container, false);
+
+        addMeetingButton = view.findViewById(R.id.addMeetingButton);
+        if (caller == 4) {
+            addMeetingButton.setVisibility(View.VISIBLE);
+        } else {
+            addMeetingButton.setVisibility(View.GONE);
+        }
+
+        addMeeting();
+
+        return view;
+    }
+
+    private void addMeeting() {
+         addMeetingButton.setOnClickListener(new View.OnClickListener() {
+             @Override
+             public void onClick(View view) {
+                 Intent intent = new Intent(getContext(), AddMeeting.class);
+                 intent.putExtra("applicationId", applicationId);
+                 startActivity(intent);
+             }
+         });
     }
 }
