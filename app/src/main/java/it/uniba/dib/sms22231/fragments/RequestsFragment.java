436032bb737a3911178fd7e519ba8dfdc82067ab
MarcelloPit <marcpit85@hotmail.it>
2023-10-18 13:05:40 +0200

    added refresh in RequestsFragment

--- app/src/main/java/it/uniba/dib/sms22231/fragments/RequestsFragment.java
+++ app/src/main/java/it/uniba/dib/sms22231/fragments/RequestsFragment.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -57,6 +58,14 @@ public class RequestsFragment extends Fragment implements RecyclerViewInterface 
                 LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getContext(), LinearLayoutManager.VERTICAL, false);
                 rec.setLayoutManager(linearLayoutManager);
                 rec.setAdapter(recad);
+                SwipeRefreshLayout swipeRefreshLayout = view.findViewById(R.id.refreshRequest);
+                swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+                    @Override
+                    public void onRefresh() {
+                        swipeRefreshLayout.setRefreshing(false);
+                        getApplications();
+                    }
+                });
             }
         });
 
