a49586a5398cfb506003dcfa7cfcf2cb6203d89b
Andrea Saracino <andrea.saracino20@gmail.com>
2023-10-25 21:37:19 +0200

    removed unused field

--- app/src/main/java/it/uniba/dib/sms22231/model/Message.java
+++ app/src/main/java/it/uniba/dib/sms22231/model/Message.java
@@ -17,7 +17,6 @@ import it.uniba.dib.sms22231.utility.TimeUtils;
 public class Message implements Serializable {
     public String id;
     public String chatId;
-    public String attachmentId;
     public String text;
     public String senderUID;
     public String thesisId;
@@ -32,10 +31,9 @@ public class Message implements Serializable {
 
 
 
-    public Message(String id, String chatId, String attachmentId, String text, String senderUID, String taskId, Date dateSent, Boolean read, Boolean sent, MessageReference messageReference) {
+    public Message(String id, String chatId, String text, String senderUID, String taskId, Date dateSent, Boolean read, Boolean sent, MessageReference messageReference) {
         this.id = id;
         this.chatId = chatId;
-        this.attachmentId = attachmentId;
         this.text = text;
         this.senderUID = senderUID;
         this.taskId = taskId;
@@ -48,7 +46,6 @@ public class Message implements Serializable {
     public Message(Map<String, Object> data) {
         id = (String) data.get("id");
         chatId = (String) data.get("chatId");
-        attachmentId = (String) data.get("attachmentId");
         text = (String) data.get("text");
         senderUID = (String) data.get("senderUID");
         thesisId = (String) data.get("thesisId");
@@ -63,7 +60,6 @@ public class Message implements Serializable {
     public Map<String, Object> toMap() {
         Map<String, Object> map = new HashMap<>();
         map.put("chatId", chatId);
-        map.put("attachmentId", attachmentId);
         map.put("text", text);
         map.put("senderUID", senderUID);
         map.put("thesisId", thesisId);
